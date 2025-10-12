8b398c8bf71add6ae3c562b0fe1f5155a5ec3af2
Danny Regan <d.regan@columbia.edu>
2025-10-12 09:38:12 -0400

    Changes from NativeTabs to Tabs. Adds createPost tab.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,66 @@
-// app/(tabs)/_layout.tsx
-import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs'
+// // app/(tabs)/_layout.tsx
+// import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs'
+// import { Ionicons } from '@expo/vector-icons'
+
+// export default function TabsLayout() {
+//     console.log("Rendering Tabs Layout")    
+//   return (
+//     <NativeTabs>
+//       {/* Account/Profile Tab */}
+//      <NativeTabs.Trigger name="feed">
+//         <Icon sf="gear" />
+//         <Label>Feed</Label>
+//       </NativeTabs.Trigger>
+//       {/* Home Tab */}
+//       <NativeTabs.Trigger name="account">
+//         <Ionicons name="person-sharp" size={20} color="black" />
+//         <Icon sf="house.fill" />
+//         <Label>Account</Label>
+//       </NativeTabs.Trigger>
+//     </NativeTabs> 
+//   )
+// }
+
+
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function TabsLayout() {
-    console.log("Rendering Tabs Layout")    
   return (
-    <NativeTabs>
-      {/* Home Tab */}
-      <NativeTabs.Trigger name="account">
-        <Icon sf="house.fill" />
-        <Label>Account</Label>
-      </NativeTabs.Trigger>
-
-      {/* Account/Profile Tab */}
-     <NativeTabs.Trigger name="feed">
-        <Icon sf="gear" />
-        <Label>Feed</Label>
-      </NativeTabs.Trigger>
-    </NativeTabs> 
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'gray',
+      }}
+    >
+      <Tabs.Screen
+        name="feed"
+        options={{
+          title: 'Feed',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="createPost"
+        options={{
+          title: 'Post',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-sharp" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
   )
 }
---
+++ app/(tabs)/createPost.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../../lib/supabase'
+import { StyleSheet, View, Alert } from 'react-native'
+import { Button, Input } from '@rneui/themed'
+import { Session } from '@supabase/supabase-js'
+
+export default function Feed() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.verticallySpaced}>
+        <Input label="(TABS)/CREATEPOST" value={'We are inside!'} disabled />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+  },
+  mt20: {
+    marginTop: 20,
+  },
+})
